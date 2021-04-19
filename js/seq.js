75b48c12b3b972b06f5123aa41b1efc7730b4ddb
17lxve <timmydavidsonkb@gmail.com>
2021-04-19 13:11:29 +0000

    production

--- js/seq.js
+++ js/seq.js
@@ -1,3 +1,14 @@
+//Answers
+var Programming = "If you're learning programming and are having difficulties;<br>"
+                + "If you're programming an app and need assistance;<br>"
+                + "If you just want to have fun with computers:<br>"
+                + "You can contact Timmy at +2250101993733 or kouadiobhegnino@gmail.com";
+var Mangas = "Interested in mangas and animes?<br>"
+            +"Want to discover pieces of art?<br>"
+            +"You like shonens, shojos, seinens, isekais, joseis?<br>"
+            +"Message Hassan at +2250779040196 or coulibalyf6@student.iugb.edu.ci"
+
+
 function swap(){
     //everything i need
     var main = document.getElementById("lis").value;
@@ -8,17 +19,17 @@ function swap(){
             break;
         case 'first' :
             //Programming
-            answerer.innerHTML = "Just code duh\nTimmy 0101993733";
+            answerer.innerHTML = Programming;
             answerer.style.display = "block";
             break;
         case 'second' :
             //Mangas
-            answerer.innerHTML  = "Here are a few links<br>Hassan"
+            answerer.innerHTML  = Mangas;
             answerer.style.display = "block";
             break;
         case 'third' :
             //Player
-            answerer.innerHTML  = "Right \nChris 0505201515"
+            answerer.innerHTML  = "Right <br>Chris<br> +2250505201515"
             answerer.style.display = "block";
             break;
         case 'first' :
